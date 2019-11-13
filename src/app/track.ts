edb6511ad7e3a0321cee0e4d2a73564691dfc644
Eiseman, Ethan <EISEME1@nationwide.com>
2019-11-13 14:51:52 -0500

    Part 4

---
+++ src/app/track.ts
@@ -0,0 +1,8 @@
+import { ProductService, ProductDescriptionComponent } from './album';
+
+export interface Track {
+    trackNumber: number;
+    trackName: string;
+    trackLength: string;
+    trackPrice: number;
+}
